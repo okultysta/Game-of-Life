f2fa07435af13e52b742cd6435474af64e13c27a
Jędrzej <251482@edu.p.lodz.pl>
2024-10-24 16:05:52 +0200

    Adding test for PlainGameOfLifeSimulator, changing algorithm for "wrapping", adding some comments

--- src/main/java/org/example/PlainGameOfLifeSimulator.java
+++ src/main/java/org/example/PlainGameOfLifeSimulator.java
@@ -2,46 +2,43 @@ package org.example;
 
 public class PlainGameOfLifeSimulator implements GameOfLifeSimulator {
 
-    public void doStep(GameOfLifeBoard board){
-        boolean [][]  current= new boolean[board.getBoard().length+2][board.getBoard().length+2];
-        for(int i=0;i<board.getBoard().length;i++){
-            for(int j=0;j<board.getBoard()[0].length;j++){
-                current[i+1][j+1]=board.getBoard()[i][j];
-            }
-        }
+    public PlainGameOfLifeSimulator() {
+    }
+
+    public void doStep(GameOfLifeBoard board) {
         //local variable containing information about number of living cells around every cell
-        int alive = 0;
+        int alive;
         //declaration of a board in which the future status of the board is stored
         boolean[][] temporary;
-        temporary = new boolean[current.length - 2][current[0].length - 2];
+        temporary = new boolean[board.getBoard().length][board.getBoard()[0].length];
         //main part of the method
-        //we take into action only the inner part of the table, we skip the outer "frame" of cells
-        //starting from second index (1), and ending on index equal to size of the temp. (smaller) table
-        wrap(current);
+        //By using rest from division by length of board we calculate how many cells around
+        //are alive(true), including these that are on the opposite side of board(wrapping)
 
-        for (int i = 1; i <= temporary.length; i++) {
-            for (int j = 1; j <= temporary[0].length; j++) {
+        for (int i = 0; i < temporary.length; i++) {
+            for (int j = 0; j < temporary[0].length; j++) {
                 alive = 0;
                 for (int k = i - 1; k <= i + 1; k++) {
                     for (int l = j - 1; l <= j + 1; l++) {
-                        if (current[k][l]) {
+                        if (board.getBoard()[(k + board.getBoard().length) % board.getBoard().length]
+                                [(l + board.getBoard()[0].length) % board.getBoard()[0].length]) {
                             alive++;
                         }
                     }
                 }
                 //if the cell is alive itself, the number must be secremented
                 //if the cell is alive and has 2 or 3 neighbours also alive, then stays alive, otherway it dies
-                if (current[i][j]) {
+                if (board.getBoard()[i][j]) {
                     switch (--alive) {
                         case (2), (3):
-                            temporary[i - 1][j - 1] = true;
+                            temporary[i][j] = true;
                             break;
                         default:
-                            temporary[i - 1][j - 1] = false;
+                            temporary[i][j] = false;
                             break;
                     }
                 } else if (alive == 3) {
-                    temporary[i - 1][j - 1] = true;
+                    temporary[i][j] = true;
                     //if the cell is dead, only 3 alive neighbours makes her alive
                 }
             }
@@ -52,36 +49,5 @@ public class PlainGameOfLifeSimulator implements GameOfLifeSimulator {
                 board.setCell(i, j, temporary[i][j]);
             }
         }
-
-
     }
-
-    private void wrap(boolean[][] board) {
-        boolean [][] temp = new boolean[board.length+2][board[0].length+2];
-        int m = board.length;
-        int n = board[0].length;
-        //upper edge
-        for (int i = 1; i < n - 1; i++) {
-            board[0][i] = board[m - 2][i];
-        }
-        //lower edge
-        for (int i = 1; i < n - 1; i++) {
-            board[m - 1][i] = board[1][i];
-        }
-        //left edge
-        for (int i = 1; i < m - 1; i++) {
-            board[i][0] = board[i][n - 2];
-        }
-        //right edge
-        for (int i = 1; i < m - 1; i++) {
-            board[i][m - 1] = board[i][1];
-        }
-        //corners
-        board[0][0] = board[m - 2][n - 2];
-        board[0][n - 1] = board[m - 2][1];
-        board[m - 1][0] = board[1][n - 2];
-        board[m - 1][n - 1] = board[1][1];
-    }
-
-
 }
--- src/test/java/org/example/GameOfLifeBoardTest.java
+++ src/test/java/org/example/GameOfLifeBoardTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//Test checking if the board is initialized properly and with different content for each initlialization.
+
 class GameOfLifeBoardTest {
-/*
+    //Test checking if the board is initialized properly and with different content for each initlialization.
     @Test
     public void testBoardInitialization() {
-        GameOfLifeBoard board1 = new GameOfLifeBoard(3, 4);
-        GameOfLifeBoard board2 = new GameOfLifeBoard(3, 4);
+        PlainGameOfLifeSimulator simulator = new PlainGameOfLifeSimulator();
+        GameOfLifeBoard board1 = new GameOfLifeBoard(3, 4, simulator);
+        GameOfLifeBoard board2 = new GameOfLifeBoard(3, 4, simulator);
         int same = 3 * 4;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 4; j++) {
@@ -18,47 +19,17 @@ class GameOfLifeBoardTest {
                     same--;
             }
         }
-
         assertNotEquals(same, 0);
     }
 
-    //Checking if the doStep() method correctly changes the layout of cells in the board.
+    //Test checking getter and setter
     @Test
-    public void doStepTest() {
-        GameOfLifeBoard board1 = new GameOfLifeBoard(3, 3);
-        boolean[][] initialBoard = board1.getBoard();
-        board1.doStep();
-        int alive;
-        boolean[][] newBoard = board1.getBoard();
-        for (int i = 1; i < initialBoard.length - 1; i++) {
-            for (int j = 1; j < initialBoard[i].length - 1; j++) {
-                alive = 0;
-                for (int k = i - 1; k <= i + 1; k++) {
-                    for (int l = j - 1; l <= j + 1; l++) {
-                        if (initialBoard[k][l]) {
-                            alive++;
-                        }
-                    }
-                }
-                if (initialBoard[i][j]) {
-                    switch (--alive) {
-                        case 2, 3:
-                            assertTrue(newBoard[i][j]);
-                            break;
-                        default:
-                            assertFalse(newBoard[i][j]);
-                            break;
-                    }
-                } else {
-                    if (alive == 3)
-                        assertTrue(newBoard[i][j]);
-                }
-
-
-            }
-        }
-
+    public void testSetCell() {
+        PlainGameOfLifeSimulator simulator = new PlainGameOfLifeSimulator();
+        GameOfLifeBoard board1 = new GameOfLifeBoard(3, 4, simulator);
+        board1.setCell(1, 1, false);
+        assertFalse(board1.getBoard()[1][1]);
+        board1.setCell(1, 1, true);
+        assertTrue(board1.getBoard()[1][1]);
     }
-
- */
 }
