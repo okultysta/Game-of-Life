2ab6eae540b5cc4a5c2e88c09b65bc938c67654d
251586 Kacper Maziarz <251586@edu.p.lodz.pl>
2024-12-09 14:58:22 +0100

    szlify4

--- Model/src/main/java/org/example/FileGameOfLifeBoardDao.java
+++ Model/src/main/java/org/example/FileGameOfLifeBoardDao.java
@@ -5,7 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
+
 
 public class FileGameOfLifeBoardDao implements Dao<GameOfLifeBoard>, AutoCloseable {
     private final String filename;
--- Model/src/main/java/org/example/GameOfLifeBoard.java
+++ Model/src/main/java/org/example/GameOfLifeBoard.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
