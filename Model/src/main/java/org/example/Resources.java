fc891401b8d931ad050c6f2eda3a4aa77a8255fb
Jędrzej <251482@edu.p.lodz.pl>
2024-12-15 20:45:35 +0100

    dodanie resources

---
+++ Model/src/main/java/org/example/Resources.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.ListResourceBundle;
+
+public class Resources extends ListResourceBundle {
+
+    @Override
+    protected Object[][] getContents() {
+        return new Object[][] {
+                {"Author1", "Kacper Maziarz"},
+                {"Author2", "Jedrzej Bartoszewski"},
+        };
+    }
+}
